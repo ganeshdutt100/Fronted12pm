52844d8d129f30dcd3e42d507562711dfdc352d3
unknown <gdutt2304@gmail.com>
2024-09-11 12:16:40 +0530

    Update React

---
+++ React Classes/src/App.jsx
@@ -0,0 +1,15 @@
+function App() {
+   return (
+    <>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non ratione, adipisci sed excepturi reiciendis fuga ullam eius voluptas, sint pariatur, asperiores facilis rem facere accusantium exercitationem nulla repellat ut? Debitis.</p>
+      <h1>Lorem ipsum dolor sit amet.</h1>
+      <img src="" alt="" />
+      <article>
+        <h1>Lorem ipsum dolor sit amet.</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat sint, dolore ipsam quas quos, quis magnam eligendi vero porro incidunt ea eius tempore minus, quia itaque qui? Quos, cumque possimus.</p>
+      </article>
+    </>
+  )
+}
+
+export default App
